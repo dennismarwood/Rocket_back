126b916128b02f6e28fbc8cce93e4adcb6d077d4
Dennis Marwood <dennismarwood@gmail.com>
2022-09-27 23:43:00 -0700

    Address bug with bad email in login. Update how error messages are returned.

--- src/pw.rs
+++ src/pw.rs
@@ -10,32 +10,38 @@ use pbkdf2::{
     // retrieve phc for user and verify
 
     pub fn verify_password(password: String, phc: String) -> Result<(), pbkdf2::password_hash::Error>{
-        let password = password.as_bytes();
-        let parsed_hash = PasswordHash::new(&phc);
-        match parsed_hash {
-            Ok(ph) => Pbkdf2.verify_password(password, &ph),
+        let password = password.as_bytes();//User provided pw
+        let parsed_hash = PasswordHash::new(&phc);//User's computed phc
+        match parsed_hash {//Confirm that phc is valid format
+            Ok(ph) => {
+                match Pbkdf2.verify_password(password, &ph) {
+                    Ok(_) => Ok(()),//Password confirmed
+                    Err(e) => return Err(e)//Password was incorrect
+                }
+            },
             Err(e) => Err(e)
         }
 
     }
 
-
+    //phc = Shorthand for a string format developed at the Password Hacking Competition. It specifies how to determine what was used to generate the hash.
+    // Hash data $<algorithm>$<salt>$<hash>
+    // $pbkdf2-sha256$i=10000,l=32$VI1K2hsmvxq1Urky3DQY/w$gE/SJ29k8+Esw5fJdxUrJHewh+hUkKhYhlMJjRzFjDQ  <--This is an example PHC
+    // algo = pbkdf2-sha256
+    // Params = i=10000,l=32 (iterations and word size?)
+    // salt = VI1K2hsmvxq1Urky3DQY/w
+    // hash = gE/SJ29k8+Esw5fJdxUrJHewh+hUkKhYhlMJjRzFjDQ
+    //The phc is safe to store in the db because it is just the algorithm used, the salt, and the hash.
     pub fn get_phc(user_password: String) -> Result<String, pbkdf2::password_hash::errors::Error>  {// -> Result<PasswordHash<'static>, pbkdf2::password_hash::errors::Error> {
         // Hash is a one way operation: PW -> Hash algorithm = Hashed value. The user's pw is never stored.
         // Salting a PW defeats rainbow tables because it gives each input to the hash a unique value.
         // (PW + Salt) -> Algorithm = Salted and hashed value.
         // OK to store salt in plain text next to hashed value.
         // Salt should be random and unique to each user (to prevent collisions on users using the same pw)
-        // Hash data $<algorithm>$<salt>$<hash>
-        // $pbkdf2-sha256$i=10000,l=32$VI1K2hsmvxq1Urky3DQY/w$gE/SJ29k8+Esw5fJdxUrJHewh+hUkKhYhlMJjRzFjDQ
-        // algo = pbkdf2-sha256
-        // Params = i=10000,l=32 (iterations and word size?)
-        // salt = VI1K2hsmvxq1Urky3DQY/w
-        // hash = gE/SJ29k8+Esw5fJdxUrJHewh+hUkKhYhlMJjRzFjDQ
-        let password = user_password.as_bytes();
-        let salt = SaltString::generate(&mut OsRng);
+        let password = user_password.as_bytes(); //User supplied
+        let salt = SaltString::generate(&mut OsRng); //System generated salt
         match Pbkdf2.hash_password(password, &salt) {
-            Ok(phc) => Ok(phc.to_string()),
+            Ok(phc) => Ok(phc.to_string()), //A PHC. It contains meta data on the encryption method, the salt, and the generated hash.
             Err(e) => Err(e)
         }
     }
--- src/session.rs
+++ src/session.rs
@@ -27,22 +27,47 @@ pub mod routes {
 
     #[post("/", format = "json", data="<credentials>")]
     //pub async fn session(conn: DbConn, credentials: Json<Credentials>, jar: &CookieJar<'_>) -> Result<Value, status::Custom<Value>> {
-    pub async fn create_session(conn: DbConn, credentials: Json<Credentials>, jar: &CookieJar<'_>, my_config: &State<EnvVariables>) -> Result<Status, Status> { 
-        let user_email = credentials.email.clone();        
-        let user_data = conn.run( |c| {
-            user::table
-            .left_join(role::table)
-            .filter(user::email.eq(user_email))
-            .select((user::phc, user::role, role::user_role.nullable()))
-            .first::<(Option<String>, i32, Option<String>)>(c)
-        }).await
-        //.map_err(|_| status::Custom(Status::Unauthorized, json!("Invalid email and/or password.")))?;
-        .map_err(|_| Status::Unauthorized)?;
-
-        verify_password(credentials.pass.clone(), user_data.0.unwrap())
-        .map_err(|_| return Status::Unauthorized)?;
+    pub async fn create_session(conn: DbConn, credentials: Json<Credentials>, jar: &CookieJar<'_>, my_config: &State<EnvVariables>) -> (Status, Value) { 
+        let user_email = credentials.email.clone();
+        let user_pass = credentials.pass.clone();
+        let user_data = match 
+            conn.run( |c| {
+                user::table
+                .left_join(role::table)
+                .filter(user::email.eq(user_email))
+                .select((user::id, user::phc, user::role, role::user_role.nullable()))
+                .first::<(i32, Option<String>, i32, Option<String>)>(c)//Throw error if 0 rows returned
+            }).await
+            {
+                Ok(x) => x,
+                Err(_) => {
+                    //.map_err(|_| status::Custom(Status::Unauthorized, json!("Invalid email and/or password.")))?;
+                    return (Status::Unauthorized, json!({
+                        "errors": [
+                            {
+                                "status": 401,
+                                "description": "The provided email/password combination is invalid or no user with that email exists."
+                            }
+                        ]
+                    }))
+                }
+        };
+        
+        match verify_password(user_pass, user_data.1.unwrap()){
+            Ok(_) =>{},
+            Err(_) => return (Status::Unauthorized, json!({//A valid email was provided but an incorrect pw.
+                "errors": [
+                    {
+                        "status": 401,
+                        "description": "The provided email/password combination is invalid or no user with that email exists."
+                    }
+                ]
+            }))
+        };
 
         let user_email = credentials.email.clone();
+
+        //User is logging in, update "last_access" column in their row.
         match conn.run(|c| {
             diesel::update(user::table)
             .filter(user::email.eq(user_email))
@@ -50,25 +75,44 @@ pub mod routes {
             .execute(c)
         }).await {
             Ok(_) => {},
-            Err(_) => return Err(Status::InternalServerError)
+            Err(e) => return (Status::InternalServerError, json!({
+                "errors": [
+                    {
+                        "status": 500,
+                        "description": format!("{}", e)
+                    }
+                ]
+            }))
         }
 
-        
+        //The user email / password combo was valid. Issue them the appropriate jwt.
         match get_jwt(
+            user_data.0,
             credentials.email.clone(),
-            user_data.1,
-            user_data.2.unwrap(),
+            user_data.2,
+            user_data.3.unwrap(),
             my_config.jwt_secret.as_ref()){
             Ok(jwt) => {
                 let mut cookie = Cookie::new("jwt", jwt.clone());
                 cookie.set_http_only(true);
                 jar.add(cookie);
-                Ok(Status::Ok)
+                return (Status::Ok, json!({
+                    "information": [
+                        {
+                            "status": 200,
+                            "description": "A jwt for the user was generated."
+                        }
+                    ]
+                }))
             }
-            Err(e) => {
-                println!("\n{}", e);
-                Err(Status::InternalServerError)
-            }
+            Err(e) => return (Status::InternalServerError, json!({
+                "errors": [
+                    {
+                        "status": 500,
+                        "description": format!("{}", e)
+                    }
+                ]
+            }))
         }
       
     }
