d4960a4945bd0966179212cf11fac2e72df242ea
Dennis Marwood <dennismarwood@gmail.com>
2022-10-02 23:12:40 -0700

    Include note about rocket local_cache

--- src/auth.rs
+++ src/auth.rs
@@ -2,6 +2,10 @@ use rocket::http::Status;
 use rocket::request::{Request, FromRequest, Outcome};
 use crate::jwt::{validate_jwt};
 use crate::models::EnvVariables;
+use rocket::serde::json::{Json, Value, json};
+use rocket::response::status;
+use crate::jsonapi::JSONAPIError;
+
 
 /* 
     pub struct BasicAuth {
@@ -75,13 +79,13 @@ impl<'r> FromRequest <'r> for Level1 {
                     Ok(claims) => {
                         match claims.role_id {
                             1 => Outcome::Success(Level1{role_id: 1}),
-                            _ => Outcome::Failure((Status::Unauthorized, ())),
+                            _ => Outcome::Failure((Status::Forbidden, ())), //Has JWT, but JWT is for a user with incorrect role privileges
                         }     
                     },
-                    Err(_) => Outcome::Failure((Status::Unauthorized, ())),
+                    Err(_) => Outcome::Failure((Status::Unauthorized, ())), //JWT invalid, probably expired
                 }
             },
-            None => return Outcome::Failure((Status::Unauthorized, ())),
+            None => return Outcome::Failure((Status::Unauthorized, ())), //Had no JWT
         }
     }
 }
@@ -90,11 +94,39 @@ pub struct ValidSession{
     pub id: i32,
 }
 
+/*
+What I wanted to do here was to specify the data that a Outcome::Failure returns.
+Unfortunately, that is not possible.
+There is a way to prevent duplicate runs of expensive / slow operations.
+https://api.rocket.rs/v0.4/rocket/request/trait.FromRequest.html#request-local-state
+We can also use this as a way to pass data back from the guard to a catcher.
+We can load a data type into the local cache. The first load of a data type is not replaced.
+Lets load up some json into the local cache.
+
+let s = Status::Unauthorized;
+let message = json!(&JSONAPIError{
+    status: s.code.to_string(), 
+    canonical: String::from(s.reason().unwrap()), 
+    title: String::from("Session token missing or invalid."),
+    detail: String::from("The JWT is not present or is no longer valid.")});
+request.local_cache(|| message);
+
+Then in a catcher we can pull that data out...
+(from user.rs)
+#[catch(default)]
+    pub fn catch_all(status: Status, req: &Request) -> Value {
+        json!(format!("{:?}", req.local_cache(|| String::from("ERROR"))));
+    }
+
+However, there is no data that is generated in this guard that cannot be just written up
+in the catcher. See the entries in JSONAPIError above.
+Even though I can send data back to the catcher there is no need to do so in this case.
+*/
 #[rocket::async_trait]
 impl<'r> FromRequest <'r> for ValidSession {
     type Error = ();
 
-    async fn from_request(request: &'r Request<'_>) -> Outcome<ValidSession, Self::Error> {
+    async fn from_request(request: &'r Request<'_>) -> Outcome<ValidSession, Self::Error> {// MyError<Value>> { 
         let secret = request.rocket().state::<EnvVariables>().unwrap().jwt_secret.clone();
         match request.cookies().get("jwt") 
         {
@@ -102,10 +134,10 @@ impl<'r> FromRequest <'r> for ValidSession {
                 match validate_jwt(unvalidated_jwt.value(), secret.as_ref()) 
                 {
                     Ok(claims) => Outcome::Success(ValidSession{id: claims.user_id}),
-                    Err(_) => {println!("You had a jwt but it was invalid");Outcome::Failure((Status::Unauthorized, ()))},
+                    Err(_) => Outcome::Failure((Status::Unauthorized, ())), //JWT is present but invalid, probably expired
                 }
             },
-            None => {println!("\nYou did not have a jwt");return Outcome::Failure((Status::Unauthorized, ()))},
+            None => Outcome::Failure((Status::Unauthorized, ())), //Had no JWT
         }
     }
 }
